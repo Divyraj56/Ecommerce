89ac51247ba45dbbe7cafe7a866d30246f48d647
Divyraj56 <khachardivyaraj0@gmail.com>
2024-06-24 09:42:33 +0530

    userDetails

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,28 @@ import Header from './components/Header';
 import { Footer } from './components/Footer';
 import { ToastContainer} from 'react-toastify'; //Toastify used for Message fancy box
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import summaryApi from './common';
 
 function App() {
+
+  const fetchUSerDetails = async() =>{
+      const dataResponse = await fetch(summaryApi.current_user.url,{
+        method: summaryApi.current_user.method,
+        credentials: 'include' , // for cookies etc
+        headers: {
+          'content-type': 'application/json',
+        },
+      })
+
+      const dataApi = await dataResponse.json()
+      console.log("datauser", dataResponse);
+  }
+
+  useEffect(()=>{
+    // user details
+    fetchUSerDetails()
+  },[])
   return (
     <>
       <ToastContainer />
